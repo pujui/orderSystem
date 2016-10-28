15b504b280b9fa2ee2d1e63a37c4bc8d71bd7688
Kim <kim.hung@uitox.com>
2016-10-28 14:49:10 +0800

    added: lineBot test

---
+++ protected/controllers/LineBotController.php
@@ -0,0 +1,15 @@
+<?php
+class LineBotController extends FrameController{
+
+    public function __construct(){
+        parent::__construct();
+    }
+    
+    public function hook(){
+        $file = fopen(dirname(__FILE__)."/../runtime/line.log","a+");
+        fwrite($file, sprintf('post:%s'.PHP_EOL, json_encode($_POST)));
+        fwrite($file, sprintf('get:%s'.PHP_EOL, json_encode($_GET)));
+        fwrite($file, sprintf('phpinput:%s'.PHP_EOL, file_get_contents("php://input")));
+        fclose($file);
+    }
+}
