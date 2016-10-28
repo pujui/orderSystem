1473d9108627c242506e7d3572e0bcd06dc02f22
Kim <kim.hung@uitox.com>
2016-10-28 14:58:38 +0800

    test 1

--- protected/controllers/LineBotController.php
+++ protected/controllers/LineBotController.php
@@ -2,14 +2,14 @@
 class LineBotController extends FrameController{
 
     public function __construct(){
-        parent::__construct();
     }
     
-    public function hook(){
+    public function actionHook(){
         $file = fopen(dirname(__FILE__)."/../runtime/line.log","a+");
         fwrite($file, sprintf('post:%s'.PHP_EOL, json_encode($_POST)));
         fwrite($file, sprintf('get:%s'.PHP_EOL, json_encode($_GET)));
         fwrite($file, sprintf('phpinput:%s'.PHP_EOL, file_get_contents("php://input")));
         fclose($file);
+        echo 'ok';
     }
 }
