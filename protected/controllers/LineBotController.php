053bbb1517884bb885fa71fc24b2a86292bea745
Kim <kim.hung@uitox.com>
2016-11-02 14:26:59 +0800

    test

--- protected/controllers/LineBotController.php
+++ protected/controllers/LineBotController.php
@@ -1,41 +1,34 @@
 <?php
-require_once dirname(__FILE__).'/../framework/LINEBot.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/Constant/Meta.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/Constant/MessageType.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/MessageBuilder.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/MessageBuilder/TextMessageBuilder.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/HTTPClient.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/HTTPClient/Curl.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/HTTPClient/CurlHTTPClient.php';
-require_once dirname(__FILE__).'/../framework/LINEBot/Exception/CurlExecutionException.php';
-
-
 class LineBotController extends FrameController{
 
     public function __construct(){
     }
     
     public function actionHook(){
-        $file = fopen(dirname(__FILE__)."/../runtime/line.log","a+");
-        fwrite($file, sprintf('post:%s'.PHP_EOL, json_encode($_POST)));
-        fwrite($file, sprintf('get:%s'.PHP_EOL, json_encode($_GET)));
-        fwrite($file, sprintf('phpinput:%s'.PHP_EOL, file_get_contents("php://input")));
-        fclose($file);
-        echo 'ok';
+        $input = json_decode(file_get_contents('php://input'), TRUE);
+        $response = [ 'result' => false ];
+        if(empty($input) || !is_array($input)){
+            $this->exitHook($response);
+        }
+        $response = [
+            'replyToken' => '',
+            'messages'   => [
+                'type' => 'text',
+                'text' => ''
+            ]
+        ];
+        $lineBotDAO = new LineBotDAO;
+        foreach ($input as $key=>&$data){
+            if($key == 0){
+                $response['message']['text'] = $data['source']['userId'].':'.$data['message']['text'];
+            }
+            $lineBotDAO->addAccessLog($data);
+        }
+        $this->exitHook($response);
     }
     
-    public function actionPush(){
-        try {
-            $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ql6H42WjDDCViHshU89XzBTG9k+LiAtuHyE9j+2Kwml3HlU6iymtzdlsz41aBglak7vjxh4t549kvUUvZfSQc1KVDobOM7izPQgzMWqym+4TR2REoyika4JWM5oZSBQf6pKBwtG7R81GKQ6mem1LCQdB04t89/1O/w1cDnyilFU=');
-            $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '99b1dea83b05a4271096a91025e7765c']);
-            
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-            $response = $bot->pushMessage('<to>', $textMessageBuilder);
-            
-            echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-        }catch (Exception $e){
-            var_export($e->getCode());
-            var_export($e->getMessage());
-        }
+    private function exitHook($response){
+        echo json_encode($response);
+        exit;
     }
 }
---
+++ protected/models/dao/LineBotDAO.php
@@ -0,0 +1,41 @@
+<?php
+class LineBotDAO extends BaseDAO{
+
+    public function addAccessLog($data){
+        $sql = "INSERT INTO LineBot.access_log
+                    (replyToken
+                    , `type`
+                    , `timestamp`
+                    , sourceType
+                    , userId
+                    , messageId
+                    , messageType
+                    , messageText
+                    , createTime
+                    , `log`
+                    )
+                VALUES
+                    (:replyToken
+                    , :type
+                    , :timestamp
+                    , :sourceType
+                    , :userId
+                    , :messageId
+                    , :messageType
+                    , :messageText
+                    , NOW()
+                    , :log ) ";
+        $this->bindQuery($sql, array(
+            ':replyToken'   => $data['replyToken'],
+            ':type'         => $data['type'],
+            ':timestamp'    => $data['timestamp'],
+            ':sourceType'   => $data['source']['type'],
+            ':userId'       => $data['source']['userId'],
+            ':messageId'    => $data['message']['id'],
+            ':messageType'  => $data['message']['type'],
+            ':messageText'  => $data['message']['text'],
+            ':log'          => json_encode($data),
+        ));
+    }
+
+}
