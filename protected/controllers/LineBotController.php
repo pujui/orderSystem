483a8f15f2597b66986eeba0fdba0db8fb7d2c41
Kim <kim.hung@uitox.com>
2016-11-04 16:12:12 +0800

    test

--- protected/controllers/LineBotController.php
+++ protected/controllers/LineBotController.php
@@ -154,7 +154,7 @@ class LineBotController extends FrameController{
         $lineBotDAO = new LineBotDAO;
         $userInfo = $lineBotDAO->findUser($userId);
         if(empty($userInfo)){
-            $response['message']['text'] = self::MESSAGE_BOT_SETTING;
+            //$response['message']['text'] = self::MESSAGE_BOT_SETTING;
         }
         $user = ['userId' => $userId, 'mode' => 'test'];
         $lineBotDAO->setUser($user);
